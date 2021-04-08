325a1e8fa1a0997e43a46b2f2c483ca6f7f345aa
Billy Soomro <billy.soomro@worldpay.com>
2021-04-08 15:57:03 +0100

    Adding initial service

---
+++ Guitars.Pact.Provider/Controllers/GuitarsController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using Guitars.Pact.Provider.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Guitars.Pact.Provider.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class GuitarsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var guitars = new List<Guitar>
+            {
+                new Guitar("PRS" , "Tremonti Signature Model", "Solid body", 6),
+                new Guitar("Gibson" , "Les Paul", "Solid body", 6),
+                new Guitar("Fender" , "Stratocaster", "Solid body", 6),
+                new Guitar("Ibanez" , "RG Series", "Solid body", 7),
+                new Guitar("ESP" , "KH-2 VINTAGE", "Solid body", 6)
+            };
+
+            return Ok(guitars);
+        }
+
+        [HttpPut]
+        public IActionResult Put(Guitar guitar)
+        {
+            return Ok(guitar);
+        }
+
+        [HttpPost]
+        public IActionResult Post(Guitar guitar)
+        {
+            return Created("api/guitars/1", guitar);
+        }
+
+        [HttpDelete]
+        public IActionResult Delete()
+        {
+            return Ok("Deleted");
+        }
+    }
+}
---
+++ Guitars.Pact.Provider/Models/Guitar.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+
+namespace Guitars.Pact.Provider.Models
+{
+    public class Guitar
+    {
+        [JsonProperty("make"), JsonRequired]
+        public string Make { get; set; }
+
+        [JsonProperty("model"), JsonRequired]
+        public string Model { get; set; }
+
+        [JsonProperty("shape"), JsonRequired]
+        public string Shape { get; set; }
+
+        [JsonProperty("strings"), JsonRequired]
+        public int Strings { get; set; }
+
+        public Guitar(string make, string model, string shape, int strings)
+        {
+            Make = make;
+            Model = model;
+            Shape = shape;
+            Strings = strings;
+        }
+    }
+}
